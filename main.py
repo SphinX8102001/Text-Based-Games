2e8fcaac484ec6d07d8de35a24c3e444059f6994
Al-Washi Sarker <124284136+SphinX8102001@users.noreply.github.com>
2024-03-08 20:32:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import os
 prompt = r'''1.Type 1 to play Wordle
 2.Type 2 to play Tic-Tac-Toe
 3.Type 3 to exit
+
 >'''
 
 loop = True
 while loop:
-    print()
     abc = input(prompt)
     os.system('cls')
     if abc == '3':
